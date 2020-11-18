2bbe26129bd9e0d9047b8578c13c4804ef18bd95
Dariusz Krygier <dariusz.krygier.programista@gmail.com>
2020-11-18 23:51:45 +0100

    Ball and paddes movements

---
+++ Unit1.h
@@ -0,0 +1,41 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TImage *player1;
+        TImage *player2;
+        TTimer *Player1Up;
+        TTimer *Player1Down;
+        TTimer *Player2Up;
+        TTimer *Player2Down;
+        TImage *table;
+        TImage *ball;
+        TTimer *TimerBall;
+        void __fastcall Player1UpTimer(TObject *Sender);
+        void __fastcall Player1DownTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall Player2UpTimer(TObject *Sender);
+        void __fastcall Player2DownTimer(TObject *Sender);
+        void __fastcall TimerBallTimer(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
